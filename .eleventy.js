e1cfe86562495a55b480e095a260f87af38d33fd
Klaas Cuvelier <klaas@showpad.com>
2020-12-03 17:45:45 +0100

    pass through cname

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,24 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const { DateTime } = require("luxon");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "LLL dd, yyyy"
-    );
-  });
+    eleventyConfig.addFilter('readableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLL dd, yyyy');
+    });
 
-  eleventyConfig.addFilter("head", (array, n) => {
-    if (n < 0) {
-      return array.slice(n);
-    }
-    return array.slice(0, n);
-  });
+    eleventyConfig.addFilter('head', (array, n) => {
+        if (n < 0) {
+            return array.slice(n);
+        }
+        return array.slice(0, n);
+    });
 
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("public");
-  return {};
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('public');
+    eleventyConfig.addPassthroughCopy('CNAME');
+    return {};
 };
