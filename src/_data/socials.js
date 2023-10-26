12201bda6247cfce4bd0b01d32ae70990e3eedf8
Klaas Cuvelier <cuvelierklaas@gmail.com>
2023-10-26 20:22:50 +0200

    feat: add correct url for bluesky

--- src/_data/socials.js
+++ src/_data/socials.js
@@ -3,7 +3,7 @@ module.exports = function () {
         socials: [
             { label: '@klaascuvelier@mstdn.io', url: 'https://mstdn.io/@klaascuvelier' },
             { label: 'X / Twitter', url: 'https://twitter.com/klaascuvelier' },
-            { label: '@klaascuvelier.bsky.social' },
+            { label: '@klaascuvelier.bsky.social', url: 'https://bsky.app/profile/klaascuvelier.bsky.social' },
             { label: 'Instagram', url: 'https://instagram.com/klaascuvelier' },
         ]
     };
