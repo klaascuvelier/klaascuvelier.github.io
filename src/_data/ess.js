54bc976b989b33b5ea1b78fd4156415d1ef6057a
Klaas Cuvelier <cuvelierklaas@gmail.com>
2023-11-20 19:49:36 +0100

    ess update

---
+++ src/_data/ess.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+    return {
+        progress: 64.6,
+        cityStridesUrl: 'https://citystrides.com/users/12376/cities/29349',
+        screenshot: '/public/images/ess/progress-2023-11-20.jpg'
+    }
+}
