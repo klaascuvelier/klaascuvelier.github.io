c0af1b856d9bc831990a735cea1e52d4f840517b
= <cuvelierklaas@gmail.com>
2024-11-04 11:51:25 +0100

    change social links order

--- src/_data/socials.js
+++ src/_data/socials.js
@@ -1,9 +1,9 @@
 module.exports = function () {
     return {
         socials: [
+            { label: 'BlueSky', url: 'https://bsky.app/profile/klaascuvelier.bsky.social' },
             { label: 'Mastodon', url: 'https://mstdn.io/@klaascuvelier' },
             { label: 'X (Twitter)', url: 'https://twitter.com/klaascuvelier' },
-            { label: 'BlueSky', url: 'https://bsky.app/profile/klaascuvelier.bsky.social' },
             { label: 'Instagram', url: 'https://instagram.com/klaascuvelier' },
         ],
     };
