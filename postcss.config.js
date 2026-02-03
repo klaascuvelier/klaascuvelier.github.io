9bb955862cd083693989700cf0c29ae5ab027df5
Klaas Cuvelier <cuvelierklaas@gmail.com>
2026-02-03 08:47:31 +0100

    chore: upgrade tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-// module.exports = {
-//     plugins: [require(`tailwindcss`)(`./tailwind.config.js`), require(`autoprefixer`)],
-// };
 module.exports = {
     plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+        '@tailwindcss/postcss': {},
         cssnano: {},
     },
 };
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    purge: {
-        content: ['src/**/*.html', 'src/**/*.njk', 'src/**/*.md'],
-    },
-    darkMode: 'class'
-};
